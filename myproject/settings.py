d1b828ca5be2f4220027ceb532a12277d9551670
PCopter <copter.phanuwat@gmail.com>
2025-04-14 02:39:25 +0700

    Add requirements.txt for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -82,16 +82,17 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': os.getenv('DB_NAME') ,
-        'USER' : os.getenv('DB_USER'),
-        'PASSWORD' : os.getenv('DB_PASSWORD'),
-        'HOST' : os.getenv('DB_HOST'),
-        'PORT' : os.getenv('DB_PORT')
-    }
+    # 'default': {
+    #     'ENGINE': 'mysql.connector.django',
+    #     'NAME': os.getenv('DB_NAME') ,
+    #     'USER' : os.getenv('DB_USER'),
+    #     'PASSWORD' : os.getenv('DB_PASSWORD'),
+    #     'HOST' : os.getenv('DB_HOST'),
+    #     'PORT' : os.getenv('DB_PORT')
+    # }
+        'default': dj_database_url.config(conn_max_age=600)
 }
 
 
